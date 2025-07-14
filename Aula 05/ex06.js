6a9b2d1470b7a4521b8b73bc1663b531b18e85cd
Leticia Moraes <ticia.moraes@gmail.com>
2025-07-14 16:42:00 -0300

    Create ex06.js

---
+++ Aula 05/ex06.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A Velocidade do seu carro é ${vel}Km/h!`)
+if (vel > 60) { //condição simples
+    console.log ("Velocidade acima do permitido. Você foi MULTADO.")
+} else
+console.log("Dirija sempre usando cinto de segurança.")
