268ed937c1464d34df8aed176b5c91497f5cd152
Leticia Moraes <ticia.moraes@gmail.com>
2025-07-17 16:30:41 -0300

    Create ex11.js

---
+++ Aula 06/ex11.js
@@ -0,0 +1,13 @@
+//var Agora = new Date()
+//var Hora = Agora.getHours()
+var Hora = 19
+console.log(`Agora são exatamente ${Hora} horas!`)
+if (Hora < 12) {
+    console.log("Bom dia!!")
+}
+else if (Hora <= 18){
+    console.log ("Boa Tarde!")
+}
+else{
+    console.log ("Boa noite!")
+}
