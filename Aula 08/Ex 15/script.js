715faa0fcfac4b5e3cb8d4db286c59f81908bf89
Leticia Moraes <ticia.moraes@gmail.com>
2025-07-31 14:35:52 -0300

    Update script.js

--- Aula 08/Ex 15/script.js
+++ Aula 08/Ex 15/script.js
@@ -7,7 +7,7 @@ var res = document.getElementById("res")
 if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
 window.alert ("[ERRO]: Faltam dados")
 } else{
-    res.innerHTML = "Contando..."
+    res.innerHTML = "Contando."
 }
     
 }
